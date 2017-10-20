334fb2b378c0d81bedc540c55b5a1f85e311ad53
ptk2109 <jade9877@hanmail.net>
2017-10-20 18:25:32 +0900

    test

--- server.js
+++ server.js
@@ -14,101 +14,101 @@
 
 
 
-	var express = require('express');
-	var app = express();
-	var http = require('http').Server(app);
-	var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get("/",function(req, res){
+	res.sendfile("client.html");
+});
+
+var count=1;
+io.sockets.on('connection', function(socket){
+	console.log('user connected: ', socket.id);
+	var name = "user" + count++;
+	var room_id;
 	
+	io.to(socket.id).emit('change name',name);
 	
-	app.get("/",function(req, res){
-		res.sendfile("client.html");
+	/**
+	* 이게 맞나....
+	* 
+	* 특정 클라이언트에게 보내기
+	* 	io.to(socket.id).emit('change name',name);
+	* 
+	* 
+	* 전체 보내기(브로드케스트)
+	* 	io.emit('receive message', msg);
+	* 
+	* 자신제외 룸 안의 유저에게 보낼때
+	* 	socket.broadcast.to(data.room_id).emit('system', msg);
+	* 
+	* 자신포함 룸 안의 유저
+	* 	io.sockets.in(room_id).emit('receive message',data);
+	*/	
+	
+	socket.emit('connection', {
+		type : 'connected'
 	});
 	
-	var count=1;
-	io.sockets.on('connection', function(socket){
-		console.log('user connected: ', socket.id);
-		var name = "user" + count++;
-		var room_id;
-		
-		io.to(socket.id).emit('change name',name);
-		
-		/**
-		* 이게 맞나....
-		* 
-		* 특정 클라이언트에게 보내기
-		* 	io.to(socket.id).emit('change name',name);
-		* 
-		* 
-		* 전체 보내기(브로드케스트)
-		* 	io.emit('receive message', msg);
-		* 
-		* 자신제외 룸 안의 유저에게 보낼때
-		* 	socket.broadcast.to(data.room_id).emit('system', msg);
-		* 
-		* 자신포함 룸 안의 유저
-		* 	io.sockets.in(room_id).emit('receive message',data);
-		*/	
-		
-		socket.emit('connection', {
-			type : 'connected'
-		});
-		
-		// 람다식 문법.. 나중에 공부하자...
-		socket.on('connection', data => {
-		
-			if(data.type === 'join') {
-				console.log("join: " + data.room);
-				socket.join(data.room);
-				
-				// depracated
-				// socket.set('room', data.room);
-				socket.room = data.room;
-				
-				socket.emit('system', {
-					message : 'welcome to chat room!!'
-				});
-				
-				socket.broadcast.to(data.room).emit('system', {
-					message : '[' + name + '] is connected'
-				});
-			}
-		
-		});
-		
-		socket.on('send message', function(name,text){
-			var msg = name + ' : ' + text;
-			var room = socket.room;
-			console.log("[Room No."+room+"]" +msg);
+	// 람다식 문법.. 나중에 공부하자...
+	socket.on('connection', data => {
+	
+		if(data.type === 'join') {
+			console.log("join: " + data.room);
+			socket.join(data.room);
 			
+			// depracated
+			// socket.set('room', data.room);
+			socket.room = data.room;
 			
-			if(room) {
-				io.sockets.in(room).emit('receive message',data);//자신포함 전체 룸안의 유저
-				//socket.broadcast.to(room).emit('receive message', msg);  // 자신제외 룸안의 유저
-			}
+			socket.emit('system', {
+				message : 'welcome to chat room!!'
+			});
 			
-			//io.emit('receive message', msg);
-		});
-		
-		socket.on('leaveRoom',function(){
-			socket.leave(socket.room);//룸퇴장
-			console.log('OUT ROOM LIST', io.sockets.adapter.rooms);
-		});
-		
-		socket.on('disconnect', function(){
-			var room = socket.room;
-			console.log("[Room No."+room+"]" +'user disconnected: ', socket.id);
-		});
+			socket.broadcast.to(data.room).emit('system', {
+				message : '[' + name + '] is connected'
+			});
+		}
 	
 	});
 	
-	
-	
-	
-	
-	
-	http.listen('3001', function(){
-		console.log("server on!");
+	socket.on('send message', function(name,text){
+		var msg = name + ' : ' + text;
+		var room = socket.room;
+		console.log("[Room No."+room+"]" +msg);
+		
+		
+		if(room) {
+			io.sockets.in(room).emit('receive message',msg);//자신포함 전체 룸안의 유저
+			//socket.broadcast.to(room).emit('receive message', msg);  // 자신제외 룸안의 유저
+		}
+		
+		//io.emit('receive message', msg);
 	});
+	
+	socket.on('leaveRoom',function(){
+		socket.leave(socket.room);//룸퇴장
+		console.log('OUT ROOM LIST', io.sockets.adapter.rooms);
+	});
+	
+	socket.on('disconnect', function(){
+		var room = socket.room;
+		console.log("[Room No."+room+"]" +'user disconnected: ', socket.id);
+	});
+
+});
+
+
+
+
+
+
+http.listen('3001', function(){
+	console.log("server on!");
+});
 
 
 
